a96e189e0ababa5b8bc900b9981ab1dc91f1bda2
Lukeboy_2002 <35039280+lukeboy2002@users.noreply.github.com>
2023-08-23 19:07:18 +0200

    create model and migration

---
+++ app/Models/Member.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+
+class Member extends Model implements HasMedia
+{
+    use HasFactory;
+    use InteractsWithMedia;
+
+    protected $fillable = [
+        'id',
+        'user_id',
+        'image',
+        'bio',
+        'city',
+        'birthday',
+        'active',
+        'points',
+        'played_games',
+        'won_games',
+        'created_at',
+        'updated_at',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
